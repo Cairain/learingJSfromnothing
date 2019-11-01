11a6937742607be33bc28d7b4e0e252d5578791e
Cairain <53271464+Cairain@users.noreply.github.com>
2019-11-01 19:15:55 +0800

    Add files via upload

---
+++ picsRotationDemo/movefunc.js
@@ -0,0 +1,29 @@
+			function  move(el,dir,seed,destination,callback){ 
+						let objel = document.getElementById(el);
+						var initdir;
+						var timer;
+						 this.timer = setInterval(() =>{ 
+							if('left right'.indexOf(dir) != -1 ){
+								initdir = objel.offsetLeft;
+								if(destination <= initdir){
+									  seed = -Math.abs(seed);
+									};
+								objel.style.left = initdir +   seed +'px';
+								if((destination > initdir && objel.offsetLeft >= destination ) || (destination < initdir && objel.offsetLeft <= destination )){
+									objel.style.left = destination + 'px';
+								};
+								if(objel.offsetLeft == destination){
+									clearInterval(this.timer);
+								}	
+						};
+						callback&&callback();
+						},
+									 100
+						);	
+					}
+		
+				
+				
+
+			
+		
